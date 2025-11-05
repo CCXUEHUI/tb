3863714dc8d9be9ec75cd381f287c13762c3b73b
CCXUEHUI <87299715+CCXUEHUI@users.noreply.github.com>
2025-11-05 11:37:10 +0800

    Delete py/step4_convert_to_v2ray.py

--- py/step4_convert_to_v2ray.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-with open('config.yaml', 'r', encoding='utf-8') as f:
-    config = yaml.safe_load(f)
-
-with open('v2.txt', 'w', encoding='utf-8') as f:
-    for proxy in config['proxies']:
-        line = f"{proxy['name']}|{proxy['server']}:{proxy['port']}"
-        f.write(line + '\n')
